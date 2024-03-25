57a80b7a5f560fa85f6143363b0fe4bc9cf4e997
nsihor <nsihor@gmail.com>
2024-03-26 01:15:49 +0200

    mob header and mob menu

---
+++ index.js
@@ -0,0 +1,15 @@
+const burgerBtn = document.querySelector('[data-action="nd-burger"]');
+const closeBtn = document.querySelector('[data-action="nd-close"]');
+const mobMenu = document.querySelector('[data-action="nd-mob-menu"]')
+
+function toggleMenu() {
+    burgerBtn.classList.toggle('nd_none');
+    closeBtn.classList.toggle('nd_none');
+    mobMenu.classList.toggle('nd_none');
+}
+
+burgerBtn.addEventListener('click', toggleMenu);
+
+closeBtn.addEventListener('click', toggleMenu);
+
+console.log('js')
