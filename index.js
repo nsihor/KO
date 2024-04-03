5284cde5a8c31df556659523a5e3116b61e46a9b
nsihor <nsihor@gmail.com>
2024-04-03 22:30:27 +0300

    update mom menu

--- index.js
+++ index.js
@@ -3,11 +3,22 @@
 const burgerBtn = document.querySelector('[data-action="nd-burger"]');
 const closeBtn = document.querySelector('[data-action="nd-close"]');
 const mobMenu = document.querySelector('[data-action="nd-mob-menu"]')
+const body = document.querySelector("body")
+
+function toggleBodyScroll() {
+    if (document.body.style.overflow === 'hidden') {
+        document.body.style.overflow = '';
+    } else {
+        document.body.style.overflow = 'hidden';
+    }
+}
 
 function toggleMenu() {
     burgerBtn.classList.toggle('nd_none');
     closeBtn.classList.toggle('nd_none');
     mobMenu.classList.toggle('nd_none');
+
+    toggleBodyScroll();
 }
 
 burgerBtn.addEventListener('click', toggleMenu);
