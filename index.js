20254007e67c18c4039400b8c043b0a6b00fc39a
nsihor <nsihor@gmail.com>
2024-04-03 15:53:03 +0300

    statistics slider

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// menu toggle
+
 const burgerBtn = document.querySelector('[data-action="nd-burger"]');
 const closeBtn = document.querySelector('[data-action="nd-close"]');
 const mobMenu = document.querySelector('[data-action="nd-mob-menu"]')
@@ -11,3 +13,52 @@ function toggleMenu() {
 burgerBtn.addEventListener('click', toggleMenu);
 
 closeBtn.addEventListener('click', toggleMenu);
+
+// slider
+
+function sliderIncrease(list, scrollAmount) {
+    list.scrollTo({
+        top: 0,
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+}
+
+function sliderDecrease(list, scrollAmount) {
+    list.scrollTo({
+        top: 0,
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+}
+
+function updateButtonsState(btnLeft, btnRight, list) {
+    btnLeft.disabled = list.scrollLeft === 0;
+    btnRight.disabled = list.scrollLeft >= (list.scrollWidth - list.clientWidth - 1);
+}
+
+// statistics slider
+
+const statisticsList = document.querySelector('[data-action="nd-statistic-list"]');
+const statisticsLeftButton = document.querySelector('[data-action="nd-statistic-btn-left"]');
+const statisticsRightButton = document.querySelector('[data-action="nd-statistic-btn-right"]');
+
+let statisticsScrollAmount = 0;
+const statisticsStep = 343;
+
+statisticsRightButton.addEventListener('click', function() {
+    if (statisticsList.scrollLeft <= (statisticsList.scrollWidth - statisticsList.clientWidth - 1)) {
+        statisticsScrollAmount += statisticsStep;
+        sliderIncrease(statisticsList, statisticsScrollAmount);
+    }
+});
+
+
+statisticsLeftButton.addEventListener('click', function() {
+    if (statisticsScrollAmount > 0) statisticsScrollAmount -= statisticsStep
+    sliderDecrease(statisticsList, statisticsScrollAmount)
+});
+
+statisticsList.addEventListener('scroll', function() {
+    updateButtonsState(statisticsLeftButton, statisticsRightButton, statisticsList)
+});
